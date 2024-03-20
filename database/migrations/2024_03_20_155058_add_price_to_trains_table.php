ba2adb128cbcc4bd34c86b30f95a2d97cd4ad86a
AntonioDiBari <antoniodibari.dev@gmail.com>
2024-03-20 17:00:30 +0100

    feat: migration for new column "price"

---
+++ database/migrations/2024_03_20_155058_add_price_to_trains_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->decimal('prezzo', $precision = 8, $scale = 2)->nullable()->after('orario_arrivo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->dropColumn('prezzo');
+        });
+    }
+};
