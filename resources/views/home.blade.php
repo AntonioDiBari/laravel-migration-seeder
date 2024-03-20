ac759adb55ed56f877d0de8bf0c585f389b93be9
AntonioDiBari <antoniodibari.dev@gmail.com>
2024-03-20 16:49:17 +0100

    feat: Trains from Model, migration to create the Trains Table

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,34 @@
 @section('title', 'Pagina iniziale')
 
 @section('main-content')
-  <section>
-    <div class="container py-4">
-      <h1>Laravel with Bootstrap + Vite</h1>
-    </div>
-  </section>
+    <section>
+        <div class="container py-4">
+            @forelse ($trains as $train)
+                <h2>Treno diretto a {{ $train->stazione_arrivo }}, in partenza da {{ $train->stazione_partenza }}
+                    delle ore
+                    {{ $train->orario_partenza }}</h2>
+                <ul>
+                    <li><b>Compagnia: </b>{{ $train->azienda }}</li>
+                    <li><b>Orario di arrivo stimato: </b>{{ $train->orario_arrivo }}</li>
+                    <li><b>Codice Treno: </b>{{ $train->codice_treno }}</li>
+                    <li><b>Numero Carrozze: </b>{{ $train->carrozze }}</li>
+                    <li><b>In ritardo: </b>
+                        @if ($train->on_time == 1)
+                            Nessun Ritardo
+                        @else
+                            Ci sono ritardi
+                        @endif
+                    </li>
+                    <li><b>Cancellato: </b>
+                        @if ($train->canceled == 1)
+                            Treno soppresso
+                        @else
+                            No
+                        @endif
+                    </li>
+                </ul>
+            @empty
+            @endforelse
+        </div>
+    </section>
 @endsection
