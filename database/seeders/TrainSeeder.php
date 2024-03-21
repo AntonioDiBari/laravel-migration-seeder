9e11ddbe0b04317f3149297afd2f0a6985c23c43
AntonioDiBari <antoniodibari.dev@gmail.com>
2024-03-21 17:57:43 +0100

    feat: TrainSeeder with file .csv

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -16,21 +16,49 @@ class TrainSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for ($i = 0; $i < 10; $i++) {
-            $train = new Train;
+        $file = fopen(__DIR__ . "/../csv/trains.csv", "r");
+        $first_row = true;
 
-            $train->azienda = $faker->company();
-            $train->stazione_partenza = $faker->city();
-            $train->stazione_arrivo = $faker->city();
-            $train->orario_partenza = $faker->date() . " " . $faker->time();
-            $train->orario_arrivo = $faker->date() . " " . $faker->time();
-            $train->codice_treno = $faker->bothify('??###?');
-            $train->anni_servizio = $faker->numberBetween(1, 25);
-            $train->carrozze = $faker->numberBetween(5, 16);
-            $train->on_time = $faker->boolean();
-            $train->canceled = $faker->boolean();
+        while (!feof($file)) {
+            $train_data = (fgetcsv($file));
+            if (!$first_row) {
+                $train = new Train;
+                $train->azienda = $train_data[0];
+                $train->stazione_partenza = $train_data[1];
+                $train->stazione_arrivo = $train_data[2];
+                $train->orario_partenza = $train_data[3];
+                $train->orario_arrivo = $train_data[4];
+                $train->codice_treno = $train_data[6];
+                $train->anni_servizio = $train_data[5];
+                $train->carrozze = $train_data[7];
+                $train->on_time = $train_data[8];
+                $train->canceled = $train_data[9];
 
-            $train->save();
+                $train->save();
+            }
+
+            $first_row = false;
+
         }
+
+
+
+
+        // for ($i = 0; $i < 10; $i++) {
+        //     $train = new Train;
+
+        //     $train->azienda = $faker->company();
+        //     $train->stazione_partenza = $faker->city();
+        //     $train->stazione_arrivo = $faker->city();
+        //     $train->orario_partenza = $faker->date() . " " . $faker->time();
+        //     $train->orario_arrivo = $faker->date() . " " . $faker->time();
+        //     $train->codice_treno = $faker->bothify('??###?');
+        //     $train->anni_servizio = $faker->numberBetween(1, 25);
+        //     $train->carrozze = $faker->numberBetween(5, 16);
+        //     $train->on_time = $faker->boolean();
+        //     $train->canceled = $faker->boolean();
+
+        //     $train->save();
+        // }
     }
 }
